8110374e47d036f85da1cfc7a32dad63c5d32d39
Ryan Garcia <47666442+LI-YUXIN-Ryan-Garcia@users.noreply.github.com>
2019-03-21 15:39:14 +0800

    Delete sign-up.php
    no need this file anymore

--- sign-up.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["username"]; ?> <br>
-Your email address is: <?php echo $_POST["email"]; ?> <br>
-Your password is: <?php echo $_POST["password"]; ?>
-
-</body>
-</html>
